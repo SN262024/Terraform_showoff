b9701c4bf9cb0582be5fb11ecba4254cd4f36b3f
SN262024 <surendrachauhan18@gmail.com>
2025-08-19 00:07:21 +0530

    addred one rg

---
+++ Git_Revision/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "177b7e12-5f03-4f63-bcd1-ed6d1d776bff"
+}
+
+resource "azurerm_resource_group" "Canda" {
+  name     = "rg-canada"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "India" {
+  name     = "rg-India"
+  location = "West Europe"
+}
